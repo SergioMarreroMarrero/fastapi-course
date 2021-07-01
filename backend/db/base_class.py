c56d2e41c3b375c7fd46e8cf7741d7b704575cc3
sergio <sergio.marrero@cornerjob.com>
2021-07-01 16:04:03 +0100

    section 2 code

--- backend/db/base_class.py
+++ backend/db/base_class.py
@@ -5,6 +5,6 @@ from sqlalchemy.ext.declarative import as_declarative, declared_attr
 class Base:
     id : Any
     __name__ : str
-
+    @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
